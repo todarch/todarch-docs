f61d66c4b6bc6e070602ae2cdfc7757ee55795c8
selimssevgi <selimssevgi@gmail.com>
2018-10-21 01:04:52 +0300

    feat: use dcp alias in the script

--- docker-compose/deploy_app.sh
+++ docker-compose/deploy_app.sh
@@ -3,7 +3,7 @@
 ########################################
 # put this on a Server
 # run chmod +x deploy_app.sh to make the script executable
-# 
+#
 # Execute this script:  ./deploy_app.sh username/imagename:$TAG
 #
 ########################################
@@ -20,7 +20,6 @@ IMAGE_NAME_WITH_TAG=$1
 IMAGE_NAME=$(echo $IMAGE_NAME_WITH_TAG | sed 's/:.*//')
 IMAGE_NAME_WITH_LATEST=$IMAGE_NAME:latest
 SERVICE_NAME=$(echo $IMAGE_NAME | cut -d "/" -f 2)
-DOCKER_COMPOSE_FILE=/home/selimssevgi/todarch-docs/docker-compose/docker-compose.yml
 
 echo "Using compose file: '$DOCKER_COMPOSE_FILE'"
 echo "Extracted image name: '$IMAGE_NAME'"
@@ -38,4 +37,5 @@ docker tag $IMAGE_NAME_WITH_TAG $IMAGE_NAME_WITH_LATEST
 docker image ls | grep "$IMAGE_NAME"
 
 echo "Restarting $SERVICE_NAME..."
-docker-compose -f $DOCKER_COMPOSE_FILE up -d --no-deps $SERVICE_NAME
+alias dcp
+dcp up -d --no-deps $SERVICE_NAME
