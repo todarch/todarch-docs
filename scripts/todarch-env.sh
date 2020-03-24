578465ba77e386a24399ee41aa2f0ad0d1f76250
selimssevgi <selimssevgi@gmail.com>
2020-03-24 21:41:55 +0100

    enable var sub

--- scripts/todarch-env.sh
+++ scripts/todarch-env.sh
@@ -1,5 +1,5 @@
 # add to your ~/.bashrc or ~/.bash_profile
-export TODARCH_DOCS='$HOME/todarch-docs'
+export TODARCH_DOCS="$HOME/todarch-docs"
 
 # add it to ~/.bash_aliases for production
 alias dcp='docker-compose -f $TODARCH_DOCS/docker-compose/docker-compose.yml -f $TODARCH_DOCS/docker-compose/docker-compose.prod.yml'
