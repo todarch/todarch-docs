ee06d6f79f13186c31048963ed5a4b44f70489ce
selimssevgi <selimssevgi@gmail.com>
2020-03-25 20:18:06 +0100

    read from env and set keycloak user/pass

--- scripts/todarch-env.sh
+++ scripts/todarch-env.sh
@@ -3,3 +3,6 @@ export TODARCH_DOCS="$HOME/todarch-docs"
 
 # add it to ~/.bash_aliases for production
 alias dcp='docker-compose -f $TODARCH_DOCS/docker-compose/docker-compose.yml -f $TODARCH_DOCS/docker-compose/docker-compose.prod.yml'
+
+: "${TD_KEYCLOAK_USER:?You must set}"
+: "${TD_KEYCLOAK_PASSWORD:?You must set}"
